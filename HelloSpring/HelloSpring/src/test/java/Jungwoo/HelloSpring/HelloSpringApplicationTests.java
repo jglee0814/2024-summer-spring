12b4b57ed1bebcd9e2fb1635667254332c152d3e
jglee0814 <jglee0814@g.skku.edu>
2024-08-26 19:32:51 +0900

    Inflearn lecture

---
+++ HelloSpring/HelloSpring/src/test/java/Jungwoo/HelloSpring/HelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package Jungwoo.HelloSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
